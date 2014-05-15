76afc8ef410d86594d7b983b5c8ce1b3721ba790
AliceCengal <cengallut@gmail.com>
2014-05-14 22:54:27 -0500

    Directive for clearing the pane.

--- src/main/resources/webpage/js/controllers.js
+++ src/main/resources/webpage/js/controllers.js
@@ -12,32 +12,8 @@ trussApp.controller('InputPaneCtrl', function($scope, $http) {
         });
     };
 
-});
+    $scope.clearInputPane = function() {
+        $scope.inputSet = new InputSet();
+    }
 
-trussApp.controller('JointTableCtrl', function($scope, Input) {
-    $scope.joints = Input.jointSet;
-});
-
-trussApp.controller('MemberTableCtrl', function($scope) {
-    $scope.members = [
-        new Member(1, 1, 2, 1),
-        new Member(2, 2, 3, 1),
-        new Member(3, 1, 3, 1)
-    ];
-});
-
-trussApp.controller('BeamTableCtrl', function($scope) {
-    $scope.beamTypes = [
-        new BeamType(1, 1.96, 30000),
-        new BeamType(2, 0.88, 30000)
-    ];
-});
-
-trussApp.controller('SampleBtnCtrl', function($scope, Input) {
-    $scope.clickSample = function() {
-        console.log("Sample Clicked");
-        $http.get("api/sample/foo").success(function(data) {
-            Input.copyFrom(data);
-        });
-    };
 });
