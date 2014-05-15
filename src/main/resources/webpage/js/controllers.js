681b600d018bf6ce0176d4d41b1aa04ca1026115
AliceCengal <cengallut@gmail.com>
2014-05-14 23:52:38 -0500

    Add input fields. Allow editing title.

--- src/main/resources/webpage/js/controllers.js
+++ src/main/resources/webpage/js/controllers.js
@@ -3,7 +3,10 @@
 var trussApp = angular.module('TrussApp', ['TrussServices']);
 
 trussApp.controller('InputPaneCtrl', function($scope, $http) {
+
     $scope.inputSet = new InputSet();
+
+    $scope.isEditingTitle = false;
 
     $scope.clickSample = function() {
         console.log("Sample Clicked");
@@ -14,6 +17,17 @@ trussApp.controller('InputPaneCtrl', function($scope, $http) {
 
     $scope.clearInputPane = function() {
         $scope.inputSet = new InputSet();
-    }
+    };
 
+    $scope.clickTitle = function() {
+        $scope.isEditingTitle = true;
+    };
+
+    $scope.titleInput = "";
+
+    $scope.doneTitleEditing = function() {
+        $scope.isEditingTitle = false;
+        $scope.inputSet.inputSetId = $scope.titleInput;
+        $scope.titleInput = "";
+    };
 });
