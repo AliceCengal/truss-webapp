0bacf0058aadb0add551656902ac60a03ac8aa52
AliceCengal <cengallut@gmail.com>
2014-05-17 14:38:45 -0500

    Added cache service

--- src/main/resources/webpage/js/services.js
+++ src/main/resources/webpage/js/services.js
@@ -2,7 +2,21 @@
 
 var trussService = angular.module('TrussServices', []);
 
-trussService.factory('Input', function($http) {
-    var inputModel = new InputSet();
-    return inputModel;
+trussService.factory('Cache', function($http) {
+
+    var cache = {
+        hasValue:false;
+    };
+
+    cache.push = function(val) {
+        cache.val = val;
+        cache.hasValue = true;
+    };
+
+    cache.get = function() { return cache.val; };
+
+    cache.clear = function() { cache.hasValue = false; }
+
+    return cache;
 });
+
