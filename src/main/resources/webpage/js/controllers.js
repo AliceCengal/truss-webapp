af606b1a427f2738373bc567419928be862aca41
AliceCengal <cengallut@gmail.com>
2014-05-17 22:46:13 -0500

    Docs

--- src/main/resources/webpage/js/controllers.js
+++ src/main/resources/webpage/js/controllers.js
@@ -4,11 +4,11 @@ var trussControllers = angular.module('TrussControllers', []);
 
 trussControllers.controller('InputPaneCtrl', function($scope, $http) {
 
-    $scope.inputSet = new InputSet();
-    $scope.inputSet.addJoint(new Joint(1, 0, 0));
-    $scope.inputSet.addMember(new Member(1, 1, 1, 0.0));
-
-    $scope.isEditingTitle = false;
+    // The InputSet that is being edited.
+    // Start off with a blank InputSet with some dummy values.
+    //
+    // InputSet
+    $scope.inputSet = (new InputSet()).populate();
 
     var compareId = function(a, b) {
         if (a.id < b.id) { return -1; }
@@ -18,7 +18,10 @@ trussControllers.controller('InputPaneCtrl', function($scope, $http) {
 
     /// BEGIN Navbar buttons
 
-    $scope.clickSample = function() {
+    // Load a sample InputSet from the server.
+    //
+    // Unit -> Unit
+    $scope.loadSample = function() {
         console.log("Sample Clicked");
         $http.get("api/sample/foo").success(function(data) {
             $scope.inputSet = new InputSet();
@@ -28,22 +31,36 @@ trussControllers.controller('InputPaneCtrl', function($scope, $http) {
         });
     };
 
-    $scope.clearInputPane = function() {
-        $scope.inputSet = new InputSet();
-        $scope.inputSet.addJoint(new Joint(1, 0, 0));
-        $scope.inputSet.addMember(new Member(1, 1, 1, 0.0));
+    // Clears all info and initialize the page with a new
+    // empty InputSet.
+    //
+    // Unit -> Unit
+    $scope.clearInputSet = function() {
+        $scope.inputSet = (new InputSet()).populate();
     };
 
     /// END Navbar buttons
 
     /// BEGIN Editing the title
 
+    // Boolean
+    $scope.isEditingTitle = false;
+
+    // Unit -> Unit
     $scope.clickTitle = function() {
         $scope.isEditingTitle = true;
     };
 
+    // The new title being edited. Initialized to empty.
+    //
+    // String
     $scope.titleInput = "";
 
+    // Grabs titleInput and set the title value from inputSet
+    // to that. Then set isEditingTitle to false and reset
+    // the editing mode.
+    //
+    // Unit -> Unit
     $scope.doneTitleEditing = function() {
         $scope.isEditingTitle = false;
         $scope.inputSet.inputSetId = $scope.titleInput;
@@ -54,10 +71,18 @@ trussControllers.controller('InputPaneCtrl', function($scope, $http) {
 
     /// BEGIN Adding new rows to the tables
 
+    // Add a new Joint to the inputSet, with its Id set
+    // to one higher than the current highest.
+    //
+    // Unit -> Unit
     $scope.addJoint = function() {
         $scope.inputSet.addJoint(new Joint($scope.inputSet.jointSet.length + 1, 0, 0));
     }
 
+    // Add a new Memner to the inputSet, with its Id set
+    // to ine higher than the current highest.
+    //
+    // Unit -> Unit
     $scope.addMember = function() {
         var firstBeam = $scope.inputSet.listBeamTypes()[0];
         var m = new Member($scope.inputSet.memberSet.length + 1, 1, 1, firstBeam.area);
@@ -65,64 +90,108 @@ trussControllers.controller('InputPaneCtrl', function($scope, $http) {
         $scope.inputSet.addMember(m);
     }
 
+    // Add a new BeamSpec to the inputSet, with its Id set
+    // to one higher than the current highest.
+    //
+    // Unit -> Unit
     $scope.addBeamSpec = function() {
-        $scope.inputSet.addBeamSpec(new BeamType($scope.inputSet.listBeamTypes().length + 1, 0, 0));
+        $scope.inputSet.addBeamSpec(
+                new BeamType($scope.inputSet.listBeamTypes().length + 1, 0, 0));
     }
 
     /// END Adding new rows to the tables
 
     /// BEGIN Diagram
 
+    // Scaling factor used to calculate the Joints' position on the diagram.
     $scope.zoom = 1;
 
+    // Return a pair containing the coordinate of the topological center
+    // of all the joints. No side-effect.
+    //
+    // Unit -> [Int, Int]
     $scope.jointCenter = function() {
         var len = $scope.inputSet.jointSet.length;
+        // arithmetic mean
         return $scope.inputSet.jointSet
             .map(function(j) { return [j.x, j.y]; })
             .map(function(xy) { return [xy[0]/len, xy[1]/len]; })
             .reduce(function(a, b) { return [(a[0] + b[0]), (a[1] + b[1])]; });
     }
 
+    // Return a pair containing the width and height of the SVG element.
+    // No side effect.
+    //
+    // Unit -> [Int, Int]
     $scope.diagramDimension = function() {
         var svg = d3.select(".truss-diagram")[0][0];
         return [svg.clientWidth, svg.clientHeight];
     }
 
+    // Do a transformation from the Joint coordinate system to the SVG
+    // coordinate system. Takes in a Joint x and returns its horizontal
+    // position on the SVG. No side effect.
+    //
+    // Int -> Int
     $scope.correctHorizontal = function(x) {
-        return ($scope.diagramDimension()[0])/2 + (x - $scope.jointCenter()[0]) * $scope.zoom;
+        return ($scope.diagramDimension()[0])/2 +
+                (x - $scope.jointCenter()[0]) * $scope.zoom;
     }
 
+    // Do a transformation from the Joint coordinate system to the SVG coordinate
+    // system. Takes in a Joint y and returns its vertical position on the SVG.
+    // No side effect.
+    //
+    // Int -> Int
     $scope.correctVertical = function(y) {
-        return ($scope.diagramDimension()[1])/2 - (y - $scope.jointCenter()[1]) * $scope.zoom;
+        return ($scope.diagramDimension()[1])/2 -
+                (y - $scope.jointCenter()[1]) * $scope.zoom;
     }
 
+    // Do the reverse transformation from `correctHorizontal()`. No side effect.
+    //
+    // Int -> Int
     $scope.decorrectHorizontal = function(X) {
-        return $scope.jointCenter()[0] + (X - $scope.diagramDimension()[0]/2)/$scope.zoom
+        return $scope.jointCenter()[0] +
+                (X - $scope.diagramDimension()[0]/2)/$scope.zoom
     }
 
+    // Do the reverse transformation from `correctVertical()`. No side effect.
+    //
+    // Int -> Int
     $scope.decorrectVertical = function(Y) {
-        return $scope.jointCenter()[1] - (Y - $scope.diagramDimension()[1]/2)/$scope.zoom
+        return $scope.jointCenter()[1] -
+                (Y - $scope.diagramDimension()[1]/2)/$scope.zoom
     }
 
+    // How factor multiplied to or divided from the zoom everytime
+    // `zoomPlus()` or `zoomMinus()` are called.
+    //
+    // Double
     $scope.zoomFactor = 1.2;
 
+    // Unit -> Unit
     $scope.zoomPlus = function() {
         $scope.zoom = $scope.zoom * $scope.zoomFactor;
     }
 
+    // Unit -> Unit
     $scope.zoomMinus = function() {
         $scope.zoom = $scope.zoom / $scope.zoomFactor;
     }
 
+    // Unit -> Int
     $scope.longerDimension = function() {
         var dims = $scope.diagramDimension();
         return (dims[0] > dims[1])? dims[0] : dims[1];
     }
 
+    // Unit -> Double
     $scope.gridInterval = function() {
         return $scope.longerDimension() / 9.5;
     }
 
+    // Unit -> [Double]
     $scope.horizontalAxisSet = function() {
         var interval = $scope.gridInterval();
         var width = $scope.diagramDimension()[0];
@@ -130,6 +199,7 @@ trussControllers.controller('InputPaneCtrl', function($scope, $http) {
             .concat(range(width/2 + interval, width, interval));
     }
 
+    // Unit -> [Double]
     $scope.verticalAxisSet = function() {
         var interval = $scope.gridInterval();
         var width = $scope.diagramDimension()[1];
@@ -141,14 +211,17 @@ trussControllers.controller('InputPaneCtrl', function($scope, $http) {
 
     /// BEGIN Tab Navigation
 
+    // Boolean
     $scope.isShowingDiagram = true;
 
+    // Unit -> Unit
     $scope.showDiagram = function() {
         if (!$scope.isShowingDiagram) {
             $scope.isShowingDiagram = true;
         }
     }
 
+    // Unit -> Unit
     $scope.showResult = function() {
         if ($scope.isShowingDiagram) {
             $scope.isShowingDiagram = false;
@@ -160,14 +233,23 @@ trussControllers.controller('InputPaneCtrl', function($scope, $http) {
 
     /// BEGIN Result Tab
 
+    // Boolean
     $scope.isWaitingForResult = false;
+
+    // ResultSet
     $scope.resultSet = {};
 
+    // Base 10 logarithm
+    //
+    // Number -> Number
     $scope.log10 = function(x) { return Math.log(x)/Math.LN10; }
 
+    // Rounds a number such that it would have a set
+    // number of significant figures.
+    //
+    // Number -> Number
     $scope.sigFig = function(x) {
         if (x === 0.0) { return 0.0; }
-        var sign = (x >= 0) ? 1 : -1;
         var ordinal = $scope.log10(Math.abs(x));
         if (-3 < ordinal && ordinal < 3) {
             return x.toPrecision(3);
@@ -176,6 +258,9 @@ trussControllers.controller('InputPaneCtrl', function($scope, $http) {
         }
     }
 
+    // Takes the InputSet and do a server call to get a ResultSet.
+    //
+    // Unit -> Unit
     $scope.computeResult = function() {
         $scope.isWaitingForResult = true;
         $http.post("api/computation", JSON.stringify($scope.inputSet))
--- src/main/resources/webpage/js/structures.js
+++ src/main/resources/webpage/js/structures.js
@@ -10,17 +10,23 @@ var Joint = (function () {
         this.isRestraintY = false;
         this.loadX = 0.0;
         this.loadY = 0.0;
-
-        this.positionString = "";
-        this.loadString = "";
     }
 
+    // Compare Id with another Joint. Zero if equal,
+    // +1 if in ascending order from left to right,
+    // negative otherwise.
+    //
+    // Joint -> Boolean
     Joint.prototype.compareId = function (other) {
         if (this.id > other.id) { return -1; }
         if (this.id < other.id) { return 1; }
         return 0;
     };
 
+    // Copies the data from another Joint object, according
+    // to the specs outlined in the server API.
+    //
+    // Joint -> Unit
     Joint.prototype.copy = function (other) {
         this.id = other.id;
         this.x = other.x;
@@ -29,23 +35,6 @@ var Joint = (function () {
         this.isRestraintY = other.isRestraintY;
         this.loadX = other.loadX;
         this.loadY = other.loadY;
-
-    };
-
-    Joint.prototype.trimStr = function (s) { return s.trim(); };
-
-    Joint.prototype.readFromEditingStrings = function () {
-        var posStrs = this.positionString.split(",").map(this.trimStr);
-        if (posStrs.length === 2) {
-            this.x = parseFloat(posStrs[0]);
-            this.y = parseFloat(posStrs[1]);
-        }
-
-        var loadStrs = this.loadString.split(",").map(this.trimStr);
-        if (loadStrs.length === 2) {
-            this.loadX = parseFloat(loadStrs[0]);
-            this.loadY = parseFloat(loadStrs[1]);
-        }
 
     };
 
@@ -61,6 +50,10 @@ var Member = (function () {
         this.elasticity = 0.0;
     }
 
+    // Copy the data from another Member, according to
+    // the specs outlined in the server API.
+    //
+    // Member -> Unit
     Member.prototype.copy = function (other) {
         this.id = other.id;
         this.jointLeft = other.jointLeft;
@@ -90,6 +83,21 @@ var InputSet = (function () {
         this.beamSet = [];
     }
 
+    // Initializes this InputSet with a Member and a Joint with
+    // dummy values.
+    //
+    // Unit -> InputSet
+    InputSet.prototype.populate = function() {
+        this.addJoint(new Joint(1, 0, 0));
+        this.addMember(new Member(1, 1, 1, 0.0));
+        return this;
+    }
+
+    // Reads the crosssection area and elasticity values from
+    // the Member and adds it into the beamSet if it is a new
+    // value. Also tags the Member with an Id.
+    //
+    // Member -> Unit
     InputSet.prototype.catalogueMember = function (member) {
         var catalogued = false;
         for (var bs in this.beamSet) {
@@ -107,12 +115,21 @@ var InputSet = (function () {
         }
     }
 
+    // Catalogue all the Members in memberSet for their area and
+    // elasticity values.
+    //
+    // Unit -> Unit
     InputSet.prototype.catalogueBeams = function () {
         for (var m in this.memberSet) {
             this.catalogueMember(this.memberSet[m]);
         }
     };
 
+    // Copy data from another InputSet, according to the specs
+    // outlined in the server API. Also populates the other fields
+    // in this class according to the data read in.
+    //
+    // InputSet -> Unit
     InputSet.prototype.copyFrom = function (other) {
         this.studentId = other.studentId;
         this.inputSetId = other.inputSetId;
@@ -133,27 +150,47 @@ var InputSet = (function () {
         console.log(this);
     }
 
+    // Adds a member into the memberSet. Also updates the beamSet.
+    //
+    // Member -> Unit
     InputSet.prototype.addMember = function (member) {
         this.memberSet.push(member);
         this.catalogueMember(member);
     }
 
+    // Adds a Joint into the jointSet.
+    //
+    // Joint -> Unit
     InputSet.prototype.addJoint = function (joint) {
         this.jointSet.push(joint);
     }
 
+    // Returns an array of BeamSets that correspond to the properties
+    // of all the Members in memberSet. There are no duplications.
+    //
+    // Unit -> [BeamSet]
     InputSet.prototype.listBeamTypes = function () {
         return this.beamSet;
     }
 
+    // Return a beam Id that corresponds to the Member.
+    //
+    // Member -> Int
     InputSet.prototype.beamTypeIdForMember = function (member) {
         return member.beamId;
     }
 
+    // Add a BeamSpec to beamSet.
+    //
+    // BeamSpec -> Unit
     InputSet.prototype.addBeamSpec = function (bs) {
         this.beamSet.push(bs);
     }
 
+    // Return a Joint in the jointSet that has this Id.
+    // Return null if no such Joint exist.
+    //
+    // Int -> Joint
     InputSet.prototype.jointForId = function (id) {
         for (var j in this.jointSet) {
             if (this.jointSet[j].id === id) {
@@ -165,6 +202,9 @@ var InputSet = (function () {
 
     var invalid = function (name) { return name === undefined || name === ""; };
 
+    // Return a name string derived from strudentId and inputSetId.
+    //
+    // Unit -> String
     InputSet.prototype.properName = function () {
         if (invalid(this.studentId) && invalid(this.inputSetId)) {
             return "Untitled";
